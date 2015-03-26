6785ebbcd5958da71ef8bab7980eed4311804eed
Patrick McSweeny <patmcsweeny@gmail.com>
2015-03-26 01:44:28 -0400

    Implemented basic image editor using CSS filters as described in #13

---
+++ js/filters.js
@@ -0,0 +1,51 @@
+var sampleImg = $('.sample-img');
+console.log(sampleImg);
+$('.toggleGrayscale').click(function(){
+  sampleImg.toggleClass('grayscale');
+  setLevels();  
+});
+
+$('.toggleSepia').click(function(){
+  sampleImg.toggleClass('sepia');
+  setLevels();  
+});
+
+$('.toggleInvert').click(function(){
+  sampleImg.toggleClass('invert');
+  setLevels();  
+});
+
+var setBrightness = $('#setBrightness');
+var setContrast = $('#setContrast');
+
+var setLevels = function(){
+  var brightness = setBrightness.val();
+  var contrast = setContrast.val();
+  var filters = 'brightness(' + brightness + '%) contrast(' + contrast + '%)';
+
+  if(sampleImg.hasClass('sepia')){
+    filters += ' sepia(1)';
+  }
+
+  if(sampleImg.hasClass('grayscale')){
+    filters += ' grayscale(1)';
+  }
+
+  if(sampleImg.hasClass('invert')){
+    filters += ' invert(1)';
+  }
+
+  sampleImg.css('filter', filters);
+  sampleImg.css('-webkit-filter', filters);
+};
+
+setBrightness.change(setLevels);
+
+setContrast.change(setLevels);
+
+$('#imageEditor').on('reset', function (){
+  setTimeout(function() {
+    sampleImg.removeClass('invert sepia grayscale')
+    setLevels();
+  }, 0);
+});
