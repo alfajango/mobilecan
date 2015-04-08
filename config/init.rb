4761037f3ffaaceb935365b5eca420f10882cd2d
Patrick McSweeny <patmcsweeny@gmail.com>
2015-04-07 22:16:25 -0400

    Bugfix

--- config/init.rb
+++ config/init.rb
@@ -4,6 +4,3 @@ configure :test do
   # rspec context looks for views relative to /spec dir unless specified
   set :views, File.dirname(__FILE__) + '/../views'
 end
-configure :production do
-  Sequel.connect(ENV['DATABASE_URL'])
-end
