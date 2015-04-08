c2c8817f6229c8601dd0599f3b31254a9dd74696
Patrick McSweeny <patmcsweeny@gmail.com>
2015-04-07 22:11:46 -0400

    Created separate views for content

--- my_app.rb
+++ my_app.rb
@@ -6,3 +6,11 @@ require './config/init.rb'
 get '/' do
   haml :index
 end
+
+routes = ['camera_still', 'camera_streaming', 'video', 'geolocate', 'input_types', 'filters', 'vibrate']
+
+routes.each do |route|
+  get "/#{route}" do
+    haml route.to_sym
+  end
+end
