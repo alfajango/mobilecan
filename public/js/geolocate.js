d2c7d1ec51c70a7f31dffbcd41a9aaf0c5365565
Patrick McSweeny <patmcsweeny@gmail.com>
2015-04-25 02:07:07 -0400

    Updated geolocate to display coordinates and map

--- public/js/geolocate.js
+++ public/js/geolocate.js
@@ -1,23 +1,26 @@
 if(! "geolocation" in navigator) {
   $('.locate').hide();
-  $('.weather').append('Geolocation is not supported in your browser.')
+  $('.geowrap').append('Geolocation is not supported in your browser.')
 }
 
 $('.locate').click(function(){
+
   navigator.geolocation.getCurrentPosition(function(position) {
+    $('.error').hide();
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
-    var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon;
-    $.getJSON(url, function(data){
-      var tempc = data.main.temp - 273.15; //kelvin to celsius
-      var tempf = tempc * 1.8 + 32;
-      var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
-      $('.tempc').text(Math.round(tempc));
-      $('.tempf').text(Math.round(tempf));
-      $('.humid').text(data.main.humidity + '%');
-      $('.icon').attr('src', icon);
-      $('.desc').append(data.weather[0].description);
-      $('.city').text(data.name);
-    });
-  });
+    var latlon = lat + ',' + lon;
+    var url = 'https://maps.googleapis.com/maps/api/staticmap?center=' + latlon +
+    '&zoom=13&size=400x300&markers=color:red%7C' + latlon;
+    $('.coords').text(lat + ', ' + lon);
+    $('.geoMap').attr('src', url);
+    $('.geolocate').show();
+  }, function(){
+    $('.error').show();
+  },
+  {
+    enableHighAccuracy: true,
+    timeout: 27000
+  }
+  );
 });
