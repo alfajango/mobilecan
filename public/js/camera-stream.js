51967de59d5be66db1b0d6cd13030537d504d975
Patrick McSweeny <patmcsweeny@gmail.com>
2015-04-27 04:03:48 -0400

    Reverted changes related to switching cameras

--- public/js/camera-stream.js
+++ public/js/camera-stream.js
@@ -1,67 +1,29 @@
-navigator.getUserMedia = ( navigator.getUserMedia ||
-                   navigator.webkitGetUserMedia ||
-                   navigator.mozGetUserMedia ||
-                   navigator.msGetUserMedia);
+  navigator.getUserMedia = ( navigator.getUserMedia ||
+                     navigator.webkitGetUserMedia ||
+                     navigator.mozGetUserMedia ||
+                     navigator.msGetUserMedia);
 
-if (typeof MediaStreamTrack !== 'undefined' ||
-    typeof MediaStreamTrack.getSources !== 'undefined') {
-  
-  var cameraIndex;
-  var cameras = [];
-  MediaStreamTrack.getSources(function(sources){
-    for(var i = 0; i < sources.length; i++){
-      if(sources[i].kind === 'video'){
-        cameras.push(sources[i].id);
-      }
+  $('.cameraOn').click(function(){
+    if (navigator.getUserMedia) {
+       navigator.getUserMedia ({ video: true },
+          // successCallback
+          function(localMediaStream) {
+             var video = document.querySelector('video');
+             video.src = window.URL.createObjectURL(localMediaStream);
+             video.play();
+
+            $('.cameraOff').click(function(){
+              localMediaStream.stop();
+              video.pause();
+            });
+          },
+
+          // errorCallback
+          function(err) {
+             console.log("The following error occured: " + err);
+          }
+       );
+    } else {
+       console.log("getUserMedia not supported");
     }
-  cameraIndex = 0;
   });
-}
-
-var cameraOnEvent = function(){
-  if (navigator.getUserMedia) {
-    navigator.getUserMedia ({ 
-      video: {
-        optional: [{sourceId: cameras[cameraIndex]}]
-      }
-    },
-
-    // successCallback
-    function(localMediaStream) {
-
-      var video = document.querySelector('video');
-      video.src = window.URL.createObjectURL(localMediaStream);
-      video.play();
-      if(cameras.length > 1){
-        $('.cameraSwitch').show();
-      }
-      
-      $('.cameraOff').click(function(){
-        localMediaStream.stop();
-        $('.cameraSwitch').hide();
-        video.pause();
-      });
-    },
-
-    // errorCallback
-    function(err) {
-      console.log("The following error occured: " + err);
-    }
-    );
-  }else{
-    console.log("Camera access is not supported");
-  }
-};
-
-$('.cameraOn').click(cameraOnEvent);
-
-var switchCameraEvent = function(){
-  if(cameraIndex == cameras.length - 1){
-    cameraIndex = 0;
-  }else{
-    cameraIndex++;
-  }
-  cameraOnEvent();
-}
-
-$('.cameraSwitch').click(switchCameraEvent);
