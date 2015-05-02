0090ee3385743a8f9946e93f3bee0f1476ff6d56
Patrick McSweeny <patmcsweeny@gmail.com>
2015-05-02 01:02:36 -0400

    Implemented shake to send feedback to demo accelerometer #3

--- my_app.rb
+++ my_app.rb
@@ -8,7 +8,7 @@ get '/' do
   haml :home
 end
 
-routes = ['battery_status', 'camera_streaming', 'camera_still', 'geolocate', 'input_types', 'filters', 'vibrate']
+routes = ['battery_status', 'camera_streaming', 'camera_still', 'geolocate', 'input_types', 'filters', 'vibrate', 'accelerometer']
 
 routes.each do |route|
   get "/#{route}" do
---
+++ public/js/accelerometer.js
@@ -0,0 +1,11 @@
+window.addEventListener('devicemotion', deviceMotionHandler, false);
+
+function deviceMotionHandler(eventData){
+  var x = eventData.accelerationIncludingGravity.x;
+  var y = eventData.accelerationIncludingGravity.y;
+  var z = eventData.accelerationIncludingGravity.z;
+  var total = (x*x + y*y + z*z);
+  var high = $('.shake').text();
+  if (total > 1000)
+    $('.shaken').text('Shaken, not stirred.');
+};
