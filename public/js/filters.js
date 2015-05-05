52071ee2783d607da1e7e4755e81d76d6886bc5b
Patrick McSweeny <patmcsweeny@gmail.com>
2015-05-04 20:28:03 -0400

    Added new filters #13

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,23 +1,57 @@
-var sampleImg = $('.sample-img');
+var body = $('body');
+
+$('#toggleInvert').click(function(){
+  body.toggleClass('invert');
+  setBodyLevels();
+});
+
 $('.toggleGrayscale').click(function(){
+  body.toggleClass('grayscale');
+  setBodyLevels();
+});
+
+var setBlur = $('#setBlur');
+var setOpacity = $('#setOpacity');
+
+var setBodyLevels = function(){
+  var blur = setBlur.val();
+  var opacity = setOpacity.val();
+  var filters = 'blur(' + blur + 'px) opacity(' + opacity + '%)';
+
+  if(body.hasClass('invert')){
+    filters += ' invert(1)';
+  }
+
+  if(body.hasClass('grayscale')){
+    filters += ' grayscale(1)';
+  }
+
+  body.css('filter', filters);
+  body.css('-webkit-filter', filters);
+};
+
+setBlur.change(setBodyLevels);
+setOpacity.change(setBodyLevels);
+var sampleImg = $('.sample-img');
+$('.toggleImageGrayscale').click(function(){
   sampleImg.toggleClass('grayscale');
-  setLevels();  
+  setImageLevels();
 });
 
 $('.toggleSepia').click(function(){
   sampleImg.toggleClass('sepia');
-  setLevels();  
+  setImageLevels();
 });
 
-$('.toggleInvert').click(function(){
-  sampleImg.toggleClass('invert');
-  setLevels();  
-});
+$('.toggleShadow').click(function(){
+  sampleImg.toggleClass('shadow');
+  setImageLevels();
+})
 
 var setBrightness = $('#setBrightness');
 var setContrast = $('#setContrast');
 
-var setLevels = function(){
+var setImageLevels = function(){
   var brightness = setBrightness.val();
   var contrast = setContrast.val();
   var filters = 'brightness(' + brightness + '%) contrast(' + contrast + '%)';
@@ -30,21 +64,21 @@ var setLevels = function(){
     filters += ' grayscale(1)';
   }
 
-  if(sampleImg.hasClass('invert')){
-    filters += ' invert(1)';
+  if(sampleImg.hasClass('shadow')){
+    filters += ' drop-shadow(12px 12px 8px #000)'
   }
 
   sampleImg.css('filter', filters);
   sampleImg.css('-webkit-filter', filters);
 };
 
-setBrightness.change(setLevels);
+setBrightness.change(setImageLevels);
 
-setContrast.change(setLevels);
+setContrast.change(setImageLevels);
 
 $('#imageEditor').on('reset', function (){
   setTimeout(function() {
     sampleImg.removeClass('invert sepia grayscale')
-    setLevels();
+    setImageLevels();
   }, 0);
 });
