741722e71f21b18b5d9d9019b9b758a773e4899a
Patrick McSweeny <patmcsweeny@gmail.com>
2015-05-12 20:58:32 -0400

    Added error catcher to streaming page

--- public/js/camera-stream.js
+++ public/js/camera-stream.js
@@ -3,24 +3,26 @@ navigator.getUserMedia = ( navigator.getUserMedia ||
                    navigator.mozGetUserMedia ||
                    navigator.msGetUserMedia);
 
-if (typeof MediaStreamTrack !== 'undefined' &&
-    typeof MediaStreamTrack.getSources !== 'undefined') {
-  
-  var cameraIndex;
-  var cameras = [];
-  MediaStreamTrack.getSources(function(sources){
-    for(var i = 0; i < sources.length; i++){
-      if(sources[i].kind === 'video'){
-        cameras.push(sources[i].id);
+if(typeof navigator.getUserMedia !== 'function'){
+  $('.stream').hide();
+  $('.stream-error').show();
+}else{
+  if (typeof MediaStreamTrack !== 'undefined' &&
+      typeof MediaStreamTrack.getSources !== 'undefined') {
+    var cameraIndex;
+    var cameras = [];
+    MediaStreamTrack.getSources(function(sources){
+      for(var i = 0; i < sources.length; i++){
+        if(sources[i].kind === 'video'){
+          cameras.push(sources[i].id);
+        }
       }
-    }
-  cameraIndex = 1;
-  });
-}
-var cameraOffEvent;
+    cameraIndex = 1;
+    });
+  }
+  var cameraOffEvent;
 
-var cameraOnEvent = function(){
-  if (navigator.getUserMedia) {
+  var cameraOnEvent = function(){
     if(typeof cameraIndex === 'undefined'){
       var video = true;
     }else{
@@ -32,10 +34,8 @@ var cameraOnEvent = function(){
     { 
       video: video
     },
-
     // successCallback
     function(localMediaStream) {
-
       var video = document.querySelector('video');
       video.src = window.URL.createObjectURL(localMediaStream);
       video.play();
@@ -49,27 +49,24 @@ var cameraOnEvent = function(){
       }
       $('.cameraOff').click(cameraOffEvent);
     },
-
     // errorCallback
     function(err) {
       console.log("The following error occured: " + err);
     }
     );
-  }else{
-    console.log("Camera access is not supported");
-  }
-};
+  };
 
-$('.cameraOn').click(cameraOnEvent);
+  $('.cameraOn').click(cameraOnEvent);
 
-var switchCameraEvent = function(){
-  if(cameraIndex == cameras.length - 1){
-    cameraIndex = 0;
-  }else{
-    cameraIndex++;
+  var switchCameraEvent = function(){
+    if(cameraIndex == cameras.length - 1){
+      cameraIndex = 0;
+    }else{
+      cameraIndex++;
+    }
+    cameraOffEvent();
+    cameraOnEvent();
   }
-  cameraOffEvent();
-  cameraOnEvent();
+
+  $('.cameraSwitch').click(switchCameraEvent);
 }
-
-$('.cameraSwitch').click(switchCameraEvent);
