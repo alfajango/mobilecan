0ebbd9f630bcb5fd31ad760cde1c6c498eccb8bc
Patrick McSweeny <patmcsweeny@gmail.com>
2015-09-24 13:52:58 -0400

    Cleanup

--- public/js/accelerometer.js
+++ public/js/accelerometer.js
@@ -3,22 +3,10 @@ window.addEventListener('deviceorientation', deviceorientationHandler, false);
 var xAxis = $('.x-axis');
 var yAxis = $('.y-axis');
 
-// var gn = new GyroNorm();
-
-//     gn.init().then(function(){
-//         gn.start(function(data){
-//           xAxis.height(Math.round(data.do.gamma));
-//           yAxis.val(Math.round(data.do.beta));
-//           $('#orientation').val(Math.round(eventData.alpha));
-//         });
-//     }).catch(function(e){
-//       // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-//     });
 function deviceorientationHandler(eventData) {
   xAxis.height(xAxisPercent(eventData.beta));
   yAxis.width(yAxisPercent(eventData.gamma));
   $('.orient').css('transform', 'rotate(' + eventData.alpha + 'deg)');
-  $('#x').text('hai');
 }
 
 var xAxisPercent = function(x){
@@ -38,5 +26,3 @@ var yAxisPercent = function(y){
 var percentize = function(dec){
   return Math.round((dec * 100)).toString() + "%";
 };
-
-console.log(xAxisPercent(0));
